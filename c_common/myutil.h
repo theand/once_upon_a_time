d75c20a23f06eb2cb563e0ae7c7401f94b7f6ad9
Heesang Chae <theand@gmail.com>
2013-01-17 11:58:45 +0900

    C로 과제할때 주로 사용하던 유틸리티 함수 모음.

---
+++ c_common/myutil.h
@@ -0,0 +1,43 @@
+#ifndef _MY_UTIL_ 
+#define _MY_UTIL_ 
+ 
+#include <assert.h> 
+#include <stdarg.h> 
+#include <stdlib.h> 
+#include <stdio.h> 
+#include <string.h> 
+#include <errno.h> 
+ 
+ 
+#define StrLess(strLeft, strRight)\ 
+    ( (*strLeft < *strRight) || (strcmp(strLeft, strRight)<0) ) 
+#define StrGreater(strLeft, strRight)\ 
+        ( (*strLeft > *strRight) || (strcmp(strLeft, strRight)>0) ) 
+#define StrEqual(strLeft, strRight)\ 
+        ( (*strLeft == *strRight) && (strcmp(strLeft, strRight)==0) ) 
+ 
+#define assertEquals( left, right )\ 
+        ( assert( (left) == (right) ) ) 
+#define assertStrEquals( left, right )\ 
+        ( assertTrue( StrEqual( (left), (right) ) )  ) 
+#define assertTrue( statement )\ 
+        ( assert( (statement) == true ) ) 
+#define assertFalse( statement )\ 
+        ( assert( (statement) == false ) ) 
+ 
+FILE *efopen(char *name, char *mode); 
+void closeFile(FILE *t); 
+void *emalloc(size_t n); 
+void eprintf(char *fmt, ...); 
+char *estrdup(const char *aStr); 
+void setProgName(const char *aStr); 
+char *getProgName(void); 
+ 
+ 
+#ifndef __cplusplus 
+enum {true=1, false=0}; 
+#endif 
+ 
+extern char *progName; 
+#endif 
+//////////////////////// End Of File ///////////////////////////// 
